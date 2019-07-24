7eb61f62db6a4aa91c84fca87f0fbef3018b0928
Teh Kok How <khteh@KHTeh-T490.biz4x.com>
2019-07-24 11:04:24 +0800

    Take MySQL connection host from environment to work with all use cases

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -hsvc-mysql $MYSQL_DATABASE < /usr/local/bin/db.sql
+echo User: $MYSQL_USER
+echo Host: $MYSQL_HOST
+echo Database: $MYSQL_DATABASE
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < /usr/local/bin/db.sql
